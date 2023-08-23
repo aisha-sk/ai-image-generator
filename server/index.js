e20b4cf737e9f0501dca000a5dd5e197c363226d
AishaSK <aishasuhail2005@gmail.com>
2023-08-23 10:22:33 -0600

    Testing for cors issue

--- server/index.js
+++ server/index.js
@@ -10,13 +10,14 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
-app.use(cors(corsOptions))
+app.use(cors)
+// app.use(cors(corsOptions))
 app.use(express.json({ limit: '50mb' }))
 
 
